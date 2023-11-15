51618a85efc294ca8c896f9def4582eff38bae98
sigrunedda <sem24@hi.is>
2023-11-15 14:56:48 +0000

    ehv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { searchProducts } from "./lib/api.js";
+import { renderFrontpage } from "./lib/ui.js";
 
 const params = new URLSearchParams(window.location.search);
 const query = params.get('query');
 searchProducts(query);
+const b = document.getElementsByName('body');
+renderFrontpage(b);
