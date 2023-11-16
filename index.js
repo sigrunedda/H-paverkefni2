f399be5f205cf975617fe99e9c68720d7b611b75
sigrunedda <sem24@hi.is>
2023-11-16 12:26:43 +0000

    villur lagaðar

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import { renderFrontpage } from "./lib/ui.js";
 const params = new URLSearchParams(window.location.search);
 const query = params.get('query');
 searchProducts(query);
-const b = document.getElementsByName('body');
-renderFrontpage(b);
+const b = document.body;
+await renderFrontpage(b);
