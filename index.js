89917bd1bd79e3b648d8bc0e8a98de79db183f7f
sigrunedda <sem24@hi.is>
2023-11-16 21:15:56 +0000

    einhverjar breytingar

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import { searchProducts } from "./lib/api.js";
 import { renderFrontpage } from "./lib/ui.js";
 
-const params = new URLSearchParams(window.location.search);
+async function initialize() {
+    const body = document.body;
+
+    const products = await searchProducts();
+
+    await renderFrontpage(body, products);
+}
+
+
+
+
+/*const params = new URLSearchParams(window.location.search);
 const query = params.get('query');
 searchProducts(query);
 const b = document.body;
-await renderFrontpage(b);
+await renderFrontpage(b);*/
