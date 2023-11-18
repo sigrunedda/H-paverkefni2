e90f19d1db40b2abf636ec5153e2b22d6bebae7e
sigrunedda <sem24@hi.is>
2023-11-18 23:17:01 +0000

    nánast tilbúin forsíðan

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
+import { empty } from "./lib/elements.js";
 import { searchProducts } from "./lib/api.js";
 import { renderFrontpage } from "./lib/ui.js";
 
-async function initialize() {
-    const body = document.body;
-
-    const products = await searchProducts();
-
-    await renderFrontpage(body, products);
-}
-
-
-
-
-/*const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.search);
 const query = params.get('query');
 searchProducts(query);
 const b = document.body;
-await renderFrontpage(b);*/
+await renderFrontpage(b);
+
+function route() {
+    const { search } = window.location;
+
+    const qs = new URLSearchParams(search);
+
+    const query = qs.get('query') ?? undefined;
+    const id = qs.get('id');
+
+    const parentElement = document.body;
+
+    if (id) {
+        renderFrontpage(parentElement);
+    } else {
+       return;
+    }
+}
+// Bregst við því þegar við notum vafra til að fara til baka eða áfram.
+window.onpopstate = () => {
+    /* TODO bregðast við */
+    empty(document.body);
+
+    route();
+};
+
+// Athugum í byrjun hvað eigi að birta.
+route();
+
