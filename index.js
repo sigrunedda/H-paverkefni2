9f2ffaad429eba3d3f68da4ff9beeb59973f1aa9
sigrunedda <sem24@hi.is>
2023-11-21 12:56:01 +0000

    komin eihv meiri virkni

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { empty } from "./lib/elements.js";
 import { searchProducts } from "./lib/api.js";
-import { renderFrontpage } from "./lib/ui.js";
+import { renderFrontpage, categorySite } from "./lib/ui.js";
 
 const params = new URLSearchParams(window.location.search);
 const query = params.get('query');
@@ -10,20 +10,19 @@ await renderFrontpage(b);
 
 function route() {
     const { search } = window.location;
-
+  
     const qs = new URLSearchParams(search);
-
+  
     const query = qs.get('query') ?? undefined;
     const id = qs.get('id');
-
     const parentElement = document.body;
-
+  
     if (id) {
         renderFrontpage(parentElement);
     } else {
-       return;
+        return;
     }
-}
+  }
 // Bregst við því þegar við notum vafra til að fara til baka eða áfram.
 window.onpopstate = () => {
     /* TODO bregðast við */
